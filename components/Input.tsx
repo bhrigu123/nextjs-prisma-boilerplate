2dd588fc8b9482a6d2606907f543e980eda4ef96
Bhrigu <captain.bhrigu@gmail.com>
2023-08-11 20:25:44 -0700

    initial set up

---
+++ components/Input.tsx
@@ -0,0 +1,17 @@
+interface InputProps {
+  placeholder: string;
+  name: string;
+  formRef: any;
+}
+
+export default function Input(props: InputProps) {
+  return (
+    <input
+      className="rounded p-4 text-xl w-full"
+      name={props.name}
+      placeholder={props.placeholder}
+      ref={props.formRef}
+      {...props.formRef}
+    />
+  );
+}
